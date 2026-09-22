5a08999498d7194294db73bca21af9b69d0bb250
K0akumaA <slam00bit@gmail.com>
2026-09-22 17:36:29 +0300

    Add files via upload

--- dice_bot.py
+++ dice_bot.py
@@ -2,7 +2,7 @@ import telebot
 import random
 import os
 
-TOKEN = os.environ.get('8905864437:AAHsmIv4z24OqdUoVQCK3fAYeesiALDdgYs')
+TOKEN = '8905864437:AAHsmIv4z24OqdUoVQCK3fAYeesiALDdgYs'
 bot = telebot.TeleBot(TOKEN)
 
 
